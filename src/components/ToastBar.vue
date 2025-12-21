f9bbff8ae918eb656e08015885434aff8df91a63
Yukai Huang <yukaihuangtw@gmail.com>
2025-12-21 11:33:57 +0800

    chroe: updated toast

--- src/components/ToastBar.vue
+++ src/components/ToastBar.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { applyAppUpdate, dismissOfflineReady, pwaState } from '../pwa'
 
 const show = computed(() => pwaState.offlineReady || pwaState.needRefresh)
+const updating = ref(false)
 
 async function onUpdate() {
-  await applyAppUpdate()
+  if (updating.value) return
+  updating.value = true
+  try {
+    await applyAppUpdate()
+  } finally {
+    updating.value = false
+  }
 }
 </script>
 
@@ -15,14 +22,21 @@ async function onUpdate() {
       <div v-if="pwaState.needRefresh" class="font-black uppercase mb-1 underline">! SYSTEM ALERT !</div>
       <div v-else class="font-black uppercase mb-1 underline">! DISK READY !</div>
       <div>
-        <span v-if="pwaState.needRefresh">NEW SYSTEM FILES DETECTED. APPLY UPDATE NOW?</span>
+        <span v-if="pwaState.needRefresh">NEW VERSION AVAILABLE. REFRESH TO UPDATE?</span>
         <span v-else>OFFLINE CACHE HAS BEEN INITIALIZED SUCCESSFULLY.</span>
       </div>
     </div>
 
     <div class="flex justify-end gap-2">
-      <button v-if="pwaState.needRefresh" class="tui-btn bg-tui-bg text-tui-text" type="button" @click="onUpdate">
-        [OK]
+      <button
+        v-if="pwaState.needRefresh"
+        class="tui-btn bg-tui-bg text-tui-text"
+        type="button"
+        :disabled="updating"
+        :class="updating ? 'opacity-60 cursor-not-allowed' : ''"
+        @click="onUpdate"
+      >
+        [REFRESH]
       </button>
       <button v-else class="tui-btn" type="button" @click="dismissOfflineReady">
         [DISMISS]
